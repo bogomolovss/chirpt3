3b779b90e0c7fb8fcd0e0430673e7f1a12e65f2c
Faburlas <44576572+bogomolovss@users.noreply.github.com>
2023-05-21 22:04:50 +0300

    Add first main page styles and backend things

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,12 +6,29 @@ import { api } from "~/utils/api";
 import { SignInButton } from "@clerk/nextjs";
 import { SignOutButton } from "@clerk/clerk-react";
 
+const CreatePostWizard = () => {
+  const { user } = useUser();
+
+  console.log(user);
+
+  if (!user) return null;
+
+  return <div className="flex gap-3 w-full">
+    <img src={user.profileImageUrl} alt="Profile image"
+      className="w-14 h-14 rounded-full" />
+    <input placeholder="Type some emojis!"
+      className="bg-transparent grow outline-none" />
+  </div>
+};
+
 const Home: NextPage = () => {
   const user = useUser();
 
-  const { data } = api.example.getAll.useQuery();
+  const { data, isLoading } = api.posts.getAll.useQuery();
 
+  if (isLoading) return <div>Loading...</div>
 
+  if (!data) return <div>No data</div>
 
   return (
     <>
@@ -21,10 +38,21 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div>
-          {!user.isSignedIn && <SignInButton />}
-          {!!user.isSignedIn && <SignOutButton />}
+      <main className="flex h-screen justify-center">
+        <div className="h-full w-full border-x border-slate-400 md:max-w-2xl">
+          <div className="flex border-b border-slate-400 p-4 ">
+            {!user.isSignedIn && <div className="flex
+            justify-center"><SignInButton /></div>}
+            {user.isSignedIn && <CreatePostWizard />}
+          </div>
+          <div className="flex flex-col">
+            {[...data].map(({ post, author }) => (
+              <div key={post.id}
+                className="p-8 border-b border-slate-400">
+                {post.content}
+              </div>
+            ))}
+          </div>
         </div>
       </main>
     </>
